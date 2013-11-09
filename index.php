991e2eae0c160a618561c45e3d42386cee4d1652
Robin Tregaskis <Robin Tregaskis>
2013-11-09 19:33:40 +0000

    stub index and css

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+function template (){
+	$index = '<html>
+<head>
+	<title>LDC-TSG2</title>
+	<link rel="stylesheet" href="css/style.css"/>
+</head>
+<body>
+<h2>Things, Stuff and Gack 2.0</h2>
+<canvas id="petri"></canvas>
+</body>
+</html>';
+	echo $index;
+}
+
+template();
+?>
