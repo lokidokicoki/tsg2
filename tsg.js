da4e93c2a50b4996f3f60a179c4e1aec4e2fdeaa
Robin Tregaskis <Robin Tregaskis>
2013-11-10 18:32:29 +0000

    render things! copy code from tsg/js/things.js

--- tsg.js
+++ tsg.js
@@ -24,10 +24,40 @@ $('#create').submit(function(event){
 });
 
 function draw(data){
+	var canvas = document.getElementById('petri');
+	var ctx = canvas.getContext('2d');
+	//reset canvas
+	canvas.width=canvas.width;
+	var scale = 1;
+	var rad360 = Math.PI * 2;
+
 	if (data && data !== undefined){
 		if (data.things){
 			for(var i=0,len=data.things.length;i<len;i++){
 				console.debug(data.things[i]);
+				var thing = data.things[i];
+				var select = 0;
+				var kid = 0;
+				if (thing.selected){
+					select = 255;
+				}
+				if (thing.kid){
+					kid = 255;
+				}
+
+				ctx.beginPath();
+				var fill='';
+				var energy = thing.energy;
+				if (energy > 200) {
+					ctx.fillStyle = "rgb(255,"+kid+","+select+")";
+				}else if (energy > 100) { 
+					ctx.fillStyle = "rgb(128,"+kid+","+select+")";
+				}else{
+					ctx.fillStyle = "rgb(64,"+kid+","+select+")";
+				}
+				ctx.arc(thing.posx*scale,thing.posy*scale, 1.5*scale, 0, rad360);
+				ctx.fill();
+
 			}
 		}
 	}
