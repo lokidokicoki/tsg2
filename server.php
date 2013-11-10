eed18c760219af2c646f8007d12c5f432776c53a
Robin Tregaskis <Robin Tregaskis>
2013-11-10 18:24:40 +0000

    get userID. on `create` getThings, pass to client

--- server.php
+++ server.php
@@ -1,13 +1,21 @@
 <?php
+include ("db.php");
+include("things.php");
+
 if(isset($_POST["action"]) && !empty($_POST["action"])){
+	$con = getConn();
+	$user=$_COOKIE["LDCTSG2UID"];
 	$action = $_POST["action"];
 	switch($action){
 	case "create":
-		echo "create";
+		$things = createThings($con, $user);
+		$retval = array('things'=>$things);
+		echo json_encode($retval);
 		break;
 	default:
 		echo "unknown action".$action;
 		break;
 	};
+	closeConn($con);
 }
 ?>
