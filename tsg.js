e4d2fd36e1f8f046dccb18c8d9def6eaeae36e8a
Robin Tregaskis <Robin Tregaskis>
2013-11-10 18:25:05 +0000

    stub handling of return data.

--- tsg.js
+++ tsg.js
@@ -5,16 +5,31 @@ $('#create').submit(function(event){
     // prevent default posting of form
     event.preventDefault();
 	// fire off the request to /server.php
-    $.post("server.php",{action:"create"})
+    $.post("server.php",{action:"create"}, 'json')
 	.done(function (data){
-		alert('done:'+data);
+		console.log('done pre data:'+data);
+		data = $.parseJSON(data);
+		console.log('done post data:'+data);
+		draw(data);
 	})
 	.fail(function () {
-		alert('fail');
-	})
-	.always(function (){
-		alert('always');
+		console.error('fail');
 	});
+	/*
+	.always(function (){
+		console.log('always');
+	});
+	*/
 
 });
+
+function draw(data){
+	if (data && data !== undefined){
+		if (data.things){
+			for(var i=0,len=data.things.length;i<len;i++){
+				console.debug(data.things[i]);
+			}
+		}
+	}
+};
 });
