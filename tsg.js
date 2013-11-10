0e798b6397e0dd842031069e3fdd77d22684795b
Robin Tregaskis <Robin Tregaskis>
2013-11-10 22:40:17 +0000

    run funtion

--- tsg.js
+++ tsg.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+var running = false;
 //handle thing creation
 $('#create').submit(function(event){
 	
@@ -7,21 +8,49 @@ $('#create').submit(function(event){
 	// fire off the request to /server.php
     $.post("server.php",{action:"create"}, 'json')
 	.done(function (data){
-		console.log('done pre data:'+data);
+		//console.log('done pre data:'+data);
 		data = $.parseJSON(data);
-		console.log('done post data:'+data);
+		//console.log('done post data:'+data);
 		draw(data);
+
+		$('#control').empty().html(data.control);
+
+		//can't work out how to tell run to trigger...
 	})
 	.fail(function () {
 		console.error('fail');
 	});
-	/*
-	.always(function (){
-		console.log('always');
-	});
-	*/
-
 });
+
+//handle thing creation
+$('#run').submit(function(event){
+	running =!running;
+	run(event);
+});
+
+function run(event){
+	console.log('run');
+    // prevent default posting of form
+	if (event && event != undefined){
+    	event.preventDefault();
+	}
+	// fire off the request to /server.php
+    $.post("server.php",{action:"run"}, 'json')
+	.done(function (data){
+		//console.log('done pre data:'+data);
+		data = $.parseJSON(data);
+		//console.log('done post data:'+data);
+		draw(data);
+
+		if(running){
+			setTimeout(run, 500);
+		}
+		
+	})
+	.fail(function () {
+		console.error('run fail');
+	});
+}
 
 function draw(data){
 	var canvas = document.getElementById('petri');
