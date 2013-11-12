0e160b41eafc890a2930ec6fe2afcf4c4dca5b05
Robin Tregaskis <Robin Tregaskis>
2013-11-12 21:21:46 +0000

    db: remote host ip

--- db.php
+++ db.php
@@ -2,6 +2,7 @@
 function getConn(){
 	$dbname="lokidoki_tsg2";
 	$dbhost="localhost";
+	//$dbhost="10.168.1.52";
 	$dbuser="lokidoki_tsg2";
 	$dbpass="kantara12";
 	$con=new mysqli($dbhost,$dbuser,$dbpass,$dbname);
