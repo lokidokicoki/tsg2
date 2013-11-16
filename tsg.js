d880963d0155d450d1f697a40fcb02c8e110679f
Robin Tregaskis <Robin Tregaskis>
2013-11-16 11:27:14 +0000

    add stuff to `draw`. render things as filled squares.

--- tsg.js
+++ tsg.js
@@ -1,34 +1,4 @@
-$(document).ready(function(){
 var running = false;
-$('#petri').on('click', click);
-//handle thing creation
-$('#create').submit(function(event){
-	
-    // prevent default posting of form
-    event.preventDefault();
-	// fire off the request to /server.php
-    $.post("server.php",{action:"create"}, 'json')
-	.done(function (data){
-		//console.log('done pre data:'+data);
-		data = $.parseJSON(data);
-		//console.log('done post data:'+data);
-		draw(data);
-
-		$('#control').empty().html(data.control);
-
-		//can't work out how to tell run to trigger...
-	})
-	.fail(function () {
-		console.error('fail');
-	});
-});
-
-//handle thing creation
-$('#run').submit(function(event){
-	running =!running;
-	run(event);
-});
-
 function run(event){
 	console.log('run');
     // prevent default posting of form
@@ -65,12 +35,27 @@ function draw(data){
 	var rad360 = Math.PI * 2;
 
 	if (data && data !== undefined){
+		ctx.fillStyle = "rgb(0,128,0)";
+		if (data.stuff){
+			for(var x=0,xlen=data.stuff.length;x<xlen;x++){
+				for(var y=0, ylen=data.stuff[x].length; y<ylen; y++){
+					if (data.stuff[x][y] === 1){ 
+						ctx.fillRect(x, y, 1, 1);
+					}
+				}
+			}
+		}
+
 		if (data.things){
 			for(var i=0,len=data.things.length;i<len;i++){
 				//console.debug(data.things[i]);
 				var thing = data.things[i];
 				var select = 0;
 				var kid = 0;
+				var fill='';
+				var energy = thing.energy;
+
+				// get selected status....
 				if (thing.selected){
 					select = 255;
 				}
@@ -78,9 +63,7 @@ function draw(data){
 					kid = 255;
 				}
 
-				ctx.beginPath();
-				var fill='';
-				var energy = thing.energy;
+				// colour dependant on energy value
 				if (energy > 200) {
 					ctx.fillStyle = "rgb(255,"+kid+","+select+")";
 				}else if (energy > 100) { 
@@ -88,9 +71,10 @@ function draw(data){
 				}else{
 					ctx.fillStyle = "rgb(64,"+kid+","+select+")";
 				}
-				ctx.arc(thing.posx*scale,thing.posy*scale, 1.5*scale, 0, rad360);
-				ctx.fill();
 
+				// render things as a square
+				// thing x,y is position of 'mouth'
+				ctx.fillRect(thing.posx - 1,thing.posy - 1, 3, 3);
 			}
 		}
 	}
@@ -117,4 +101,34 @@ function click(e){
 		console.error('run fail');
 	});
 };
+$(document).ready(function(){
+$('#petri').on('click', click);
+//handle thing creation
+$('#create').submit(function(event){
+	
+    // prevent default posting of form
+    event.preventDefault();
+	// fire off the request to /server.php
+    $.post("server.php",{action:"create"}, 'json')
+	.done(function (data){
+		//console.log('done pre data:'+data);
+		data = $.parseJSON(data);
+		//console.log('done post data:'+data);
+		draw(data);
+
+		$('#control').empty().html(data.control);
+
+		//can't work out how to tell run to trigger...
+	})
+	.fail(function () {
+		console.error('fail');
+	});
+});
+
+//handle thing creation
+$('#run').submit(function(event){
+	running =!running;
+	run(event);
+});
+
 });
