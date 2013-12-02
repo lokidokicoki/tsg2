215ae3652e8e2ff899d67594e20fa67c753f6e32
Robin Tregaskis <me@lokidokicoki.co.uk>
2013-12-02 19:24:05 +0000

    tidy up thing table, add defaults

--- tsg2_tables.sql
+++ tsg2_tables.sql
@@ -3,13 +3,13 @@ use lokidoki_tsg2;
 create table if not exists thing (
 	thingID integer not null auto_increment,
 	userID char(255) not null,
-	posx integer not null,
-	posy integer not null,
-	age integer,
-	direction integer,
+	x integer not null,
+	y integer not null,
+	age integer not null default '0',
+	direction integer not null default '0',
 	energy float(10,2),
 	genes text,
 	ancestors text,
 	primary key (thingID, userID),
-	index thing_idx (posx,posy)
+	index thing_idx (x,y)
 );
